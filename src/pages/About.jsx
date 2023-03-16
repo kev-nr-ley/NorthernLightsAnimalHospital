5fed08bf1c9d84f1904e0fa76793e8c7417a4fc9
discountratio <discountratio@gmail.com>
2023-03-16 11:52:28 +0700

    Update About.jsx

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import AboutHero from "@sections/about/AboutHero";
 import Meet from "@sections/about/Meet";
 import Bouncy from "@sections/about/Bouncy";
+import {Box} from "@chakra-ui/react";
 import Testimonials from "@sections/about/Testimonials";
 import { Parallax } from "react-parallax";
 import Page from "@components/Page";
@@ -14,6 +15,7 @@ export default function About(props) {
         strength={300}
         bgImageStyle={{ width: "100vw", height: "90vh", objectFit: "cover" }}
         >
+
         <AboutHero h='80vh'/>
       </Parallax>
 
